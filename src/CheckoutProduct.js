84d14c6e2f4cb594921f6ba4fb6859569bb9d358
Bharat Goyal <bgoyal1471@gmail.com>
2021-11-19 18:20:22 +0530

    Basket Update

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,11 +1,22 @@
 import React from "react";
 import "./CheckoutProduct.css";
+import { useStateValue } from "./StateProvider";
+
 function CheckoutProduct({ id, image, title, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
+
+  const removeFromBasket = () => {
+    //remove the item from basket
+    dispatch({
+      type: "REMOVE_FROM_BASKET",
+      id: id,
+    });
+  };
   return (
     <div className="checkoutProduct">
       <img className="checkoutProduct__image" src={image} />
 
-      <div className="checkout__Product__info">
+      <div className="checkoutProduct__info">
         <p className="checkoutProduct__title">{title}</p>
         <p className="checkoutProduct__price">
           <small> ₹ </small>
@@ -15,10 +26,10 @@ function CheckoutProduct({ id, image, title, price, rating }) {
           {Array(rating)
             .fill()
             .map((_, i) => (
-              <p>🌟</p>
+              <p>⭐</p>
             ))}
         </div>
-        <button>Remove from Basket</button>
+        <button onClick={removeFromBasket}>Remove from Basket</button>
       </div>
     </div>
   );
--- src/reducer.js
+++ src/reducer.js
@@ -15,6 +15,21 @@ const reducer = (state, action) => {
         ...state,
         basket: [...state.basket, action.item],
       };
+    case "REMOVE_FROM_BASKET":
+      const index = state.basket.findIndex(
+        (basketitem) => basketitem.id === action.id
+      );
+      let newBasket = [...state.basket];
+      if (index >= 0) {
+        newBasket.splice(index, 1);
+      } else {
+        console.warn("Cannot remove product. Not in cart");
+      }
+      return {
+        ...state,
+        basket: newBasket,
+      };
+
     default:
       return state;
   }
