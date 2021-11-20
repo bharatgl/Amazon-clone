3c78222e670c56fbfb52115f8c237214b4c9f280
Bharat Goyal <bgoyal1471@gmail.com>
2021-11-20 22:10:25 +0530

    updates

--- src/login.js
+++ src/login.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./Login.css";
+import "./login.css";
 import { Link, useHistory } from "react-router-dom";
 import { auth } from "./firebase";
 import {
