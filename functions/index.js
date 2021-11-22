5dad1d9f125943ea62bf94b583bec756b1974dd4
Bharat Goyal <bgoyal1471@gmail.com>
2021-11-23 02:39:09 +0530

    Integrated_Order_page_to_Payment

--- functions/index.js
+++ functions/index.js
@@ -1,40 +1,32 @@
 const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
-const { response } = require("express");
 const stripe = require("stripe")(
   "sk_test_51JyLaySDM9GCM5aoLkgQ5o5dAZsmhEgbfh1j96FlzEOzZPgBNruWxXrDQoXcYdVfqhoj4Maf6Jo44TLVBLOC6ygp00ZPb7qNb0"
 );
 
-//API
-
-// App config
 const app = express();
-
-//Middlewares
 app.use(cors({ origin: true }));
 app.use(express.json());
 
-//API routes
-app.get("/", (request, response) => response.status(200).send("hello world"));
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
 
-app.post("/payments/create", async (request, response) => {
-  const total = request.query.total;
+app.post("/payments/create", async (req, res) => {
+  const total = req.query.total;
 
-  console.log("Payment Request Received BOOM!! for this amount >>> ", total);
+  console.log(total);
 
-  const paymentIntent = await stripe.paymentIntens.create({
-    amount: total, //subunits of the currency
-    currency: "rupee",
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total,
+    currency: "usd",
   });
 
-  response.status(201).send({
+  res.status(201).send({
     clientSecret: paymentIntent.client_secret,
   });
 });
 
-// - Listen command
 exports.api = functions.https.onRequest(app);
-
-//Example Endpoint
-//http://localhost:5001/clone-b540c/us-central1/api
+// app.listen(5000);
